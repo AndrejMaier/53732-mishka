ab68ed8e5111d836b23c0fa1c8ee8bf1151028be
Андрей <88maier@mail.ru>
2018-05-22 20:22:56 +0200

    Написал код открытия модального окна

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,13 +1,19 @@
 var popularCardOrder = document.querySelector('.product-card-full__button');
-var card = document.querySelector('.product-cards__in-card');
 var popup = document.querySelector('.modal');
+var cards = document.querySelectorAll('.product-cards__in-card');
 
-popularCardOrder.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  popup.classList.add('modal--active');
-});
+if (popularCardOrder) {
+  popularCardOrder.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    popup.classList.add('modal--active');
+  })
+}
 
-card.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  popup.classList('modal--active');
-});
+if (cards) {
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', function (evt) {
+      evt.preventDefault();
+      popup.classList.add('modal--active');
+    })
+  }
+}
