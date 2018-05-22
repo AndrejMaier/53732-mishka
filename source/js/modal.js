27646a94d8f22f2bbb6d41c09d675d7726f52e6f
Андрей <88maier@mail.ru>
2018-05-23 00:07:39 +0200

    Сделал модальное окно рабочее на js

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,6 +1,7 @@
 var popularCardOrder = document.querySelector('.product-card-full__button');
 var popup = document.querySelector('.modal');
 var cards = document.querySelectorAll('.product-cards__in-card');
+var close = document.querySelector('.modal__close');
 
 if (popularCardOrder) {
   popularCardOrder.addEventListener('click', function(evt) {
@@ -17,3 +18,15 @@ if (cards) {
     })
   }
 }
+
+close.addEventListener('click', function(evt) {
+  evt.preventDefault;
+  popup.classList.remove('modal--active');
+})
+
+window.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    popup.classList.remove('modal--active');
+  }
+})
